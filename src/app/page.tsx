80ffed5ebe0b4f17b1726eca87bb7018e94f4b8c
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2024-01-09 18:27:12 -0500

    able to track and plot mouse movements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,58 @@
+'use client'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Home () {
+
+  let sessionData:{
+    x: number,
+    y: number
+  }[] = []
+
+  function handleMouseMove (e: MouseEvent) {
+    console.log("collecting mouse data....")
+    let xCord = e.clientX
+    let yCord = e.clientY
+    let node = {
+      x: xCord,
+      y: yCord
+    }
+    sessionData.push(node)
+  }
+
+  function handleStart () {
+    window.addEventListener("mousemove", handleMouseMove)
+  }
+
+  function handleStop () {
+    console.log("stoped collecting mouse data")
+    console.log(sessionData)
+    window.removeEventListener("mousemove", handleMouseMove)
+    mapMouseMovement()
+  }
+
+  function mapMouseMovement () {
+    let canvas = document.getElementById('canvas') as HTMLCanvasElement
+    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    if (!canvas) return
+    let context = canvas.getContext('2d')
+    if (!context) return
+
+    context.fillStyle = "blue"
+    sessionData.forEach(node => {
+      context?.fillRect(node.x, node.y, 1, 1)
+    })
+  }
+
   return (
     <main className={styles.main}>
+
+      <div className={styles.buttonwrapper}>
+        <button onClick={handleStart}>start</button>
+        <button onClick={handleStop}>stop</button>
+      </div>
+
+      <canvas id="canvas" className={styles.mapcanvas}></canvas>
 
     </main>
   )
